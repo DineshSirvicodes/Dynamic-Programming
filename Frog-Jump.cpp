3bbecacdeb3967a729e622a1154843ad507eea29
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2024-07-26 00:32:10 +0530

    Update Frog-Jump.cpp

--- Frog-Jump.cpp
+++ Frog-Jump.cpp
@@ -16,7 +16,7 @@ int frogJump(int n, vector<int> &heights)
     return func(n-1,heights);
 }
 
-// Memoization 
+// Memoization  TC - O(N)  , SC  - O(N) + O(N) - recursive stack space
 #include <bits/stdc++.h> 
 int func(int ind , vector<int>& heights,vector<int>&dp){
     if(ind == 0) return 0;
@@ -33,4 +33,42 @@ int frogJump(int n, vector<int> &heights)
 {
     vector<int> dp(n+1,-1);
     return func(n-1,heights,dp);
+}
+
+//Tabulation TC - O(N) , SC - O(N)
+#include <bits/stdc++.h> 
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    vector<int> dp(n,0);
+    dp[0] = 0;
+    for(int i = 1; i<n ; i++){
+        int left = dp[i-1] + abs(heights[i]-heights[i-1]);
+        int right = INT_MAX;
+        if(i>1){
+            right = dp[i-2] + abs(heights[i] - heights[i-2]);
+        }
+        dp[i] = min(left,right);
+    }
+    return dp[n-1];
+}
+
+// Space optimization TC - O(N) , SC - O(1)
+#include <bits/stdc++.h> 
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    int prev = 0;
+    int prev2 = 0;
+    for(int i = 1; i<n ; i++){
+        int left = prev + abs(heights[i]-heights[i-1]);
+        int right = INT_MAX;
+        if(i>1){
+            right = prev2 + abs(heights[i] - heights[i-2]);
+        }
+        int curr = min(left,right);
+        prev2 = prev;
+        prev = curr;
+    }
+    return prev; // at n-1 will be prev
 }
