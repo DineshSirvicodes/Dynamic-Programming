7a325e9cb77a9ad9197c6cc03c4d2840f33492c0
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2024-07-29 00:13:57 +0530

    Update Ninja's-Training.cpp

--- Ninja's-Training.cpp
+++ Ninja's-Training.cpp
@@ -31,3 +31,60 @@ int ninjaTraining(int n, vector<vector<int>> &points)
     return func(n-1,3,points,dp);
 }
 
+// Tabulation TC - O(N*4*3) SC - O(N*4)
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    // define dp 2d array of size N*4; where N is the no. of days and 4 represents the tasks cols 0 , 1, 2 ,3
+    vector<vector<int>> dp(n,vector<int>(4,0));
+    dp[0][0] = max(points[0][1] , points[0][2]);
+    dp[0][1] = max(points[0][0] , points[0][2]);
+    dp[0][2] = max(points[0][0] , points[0][1]);
+    dp[0][3] = max(points[0][0] , max(points[0][1],points[0][2]));
+    
+    // start from day 1 to n-1
+    for(int day = 1 ; day<n ; day++){
+        for(int last = 0 ; last<4 ; last++){
+            dp[day][last] = 0;
+            // iterate thru the task for the given day
+
+            for(int task = 0 ;task<=2 ;task++){
+                if(task!=last){
+                    int point = points[day][task] + dp[day-1][task];
+                    dp[day][last] = max(dp[day][last],point);
+                }
+            }
+        }
+    }
+    return dp[n-1][3];
+    
+}
+
+// Space Optimization  TC - O(N*4)*3  SC - O(4) -> O(1) constant time
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    
+    vector<int> prev(4,0);
+    prev[0] = max(points[0][1] , points[0][2]);
+    prev[1] = max(points[0][0] , points[0][2]);
+    prev[2] = max(points[0][0] , points[0][1]);
+    prev[3] = max(points[0][0] , max(points[0][1],points[0][2]));
+    
+    // start from day 1 to n-1
+    for(int day = 1 ; day<n ; day++){
+        vector<int> temp(4,0);
+        for(int last = 0 ; last<4 ; last++){
+            temp[last] = 0;
+            // iterate thru the task for the given day
+
+            for(int task = 0 ;task<=2 ;task++){
+                if(task!=last){
+                    int point = points[day][task] + prev[task];
+                    temp[last] = max(temp[last],point);
+                }
+            }
+        }
+        prev = temp;
+    }
+    return prev[3];
+    
+}
