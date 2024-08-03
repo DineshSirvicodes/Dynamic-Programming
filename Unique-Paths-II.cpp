6a98bf8a59636ec033f50cabf2ef7d60b454b1c5
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2024-08-03 22:07:12 +0530

    Update Unique-Paths-II.cpp

--- Unique-Paths-II.cpp
+++ Unique-Paths-II.cpp
@@ -68,3 +68,32 @@ public:
         return dp[m-1][n-1];
     }
 };
+// Space Optimization TC = O(n*m) Sc - o(N), only to store one row
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        // vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<int>prev(n,0);
+        for(int i = 0 ;i<m; i++){
+            vector<int>temp(n,0);
+            for(int j = 0 ;j<n; j++){
+                if(obstacleGrid[i][j] == 1) {
+                    temp[j] = 0;
+                }
+                else if(i == 0 && j == 0) {
+                    temp[j] = 1;
+                }
+                else{
+                    int up = 0, left = 0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = temp[j-1];
+                    temp[j] = up+left;
+                }    
+            }
+            prev = temp;
+        }
+        return prev[n-1];
+    }
+};
