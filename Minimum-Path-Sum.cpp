631273c0eddbe27e5555e3aa68a19010d95056c8
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2024-08-04 22:20:18 +0530

    Update Minimum-Path-Sum.cpp

--- Minimum-Path-Sum.cpp
+++ Minimum-Path-Sum.cpp
@@ -21,4 +21,38 @@ public:
     }
 };
 
-// Tabulation
+// Tabulation TC - O(N*M) SC - O(N*M)
+
+class Solution {
+public:
+    int findPathSum(int i ,int j,vector<vector<int>>&grid,vector<vector<int>>&dp){
+        if(i== 0 && j == 0) return grid[0][0];
+        if(i<0 || j<0) return INT_MAX;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = INT_MAX, left = INT_MAX;
+        if(i>0) up = grid[i][j] + findPathSum(i-1,j,grid,dp);
+        if(j>0) left = grid[i][j] + findPathSum(i,j-1,grid,dp);
+        return dp[i][j] = min(up,left);
+       
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return findPathSum(m-1,n-1,grid,dp);
+        // dp[0][0] = grid[0][0];
+        for(int i = 0 ;i<m ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if(i == 0 && j == 0) dp[i][j] = grid[i][j];
+                else{
+                    int up = INT_MAX, left = INT_MAX;
+                    if(i>0) up = grid[i][j] + dp[i-1][j];
+                    if(j>0) left = grid[i][j] + dp[i][j-1];
+                    dp[i][j] = min(up,left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
