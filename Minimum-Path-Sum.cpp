33fc4810947e6876fc80e8e17622b1363f33d298
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2024-08-04 22:25:33 +0530

    Update Minimum-Path-Sum.cpp

--- Minimum-Path-Sum.cpp
+++ Minimum-Path-Sum.cpp
@@ -56,3 +56,27 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+// Space Optimization
+int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return findPathSum(m-1,n-1,grid,dp);
+        // dp[0][0] = grid[0][0];
+        vector<int> prev(n,0);
+        for(int i = 0 ;i<m ; i++){
+            vector<int> temp(n,0);
+            for(int j = 0 ; j<n ; j++){
+                if(i == 0 && j == 0) temp[j] = grid[i][j];
+                else{
+                    int up = INT_MAX, left = INT_MAX;
+                    if(i>0) up = grid[i][j] + prev[j];
+                    if(j>0) left = grid[i][j] + temp[j-1];
+                    temp[j] = min(up,left);
+                }
+            }
+            prev = temp;
+        }
+        return prev[n-1];
+}
