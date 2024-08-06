bab749635e470bb265c0bccdbf95852f5623ddf4
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2024-08-06 22:29:57 +0530

    Update Minimum-Path-Sum-in-A-Triangle-Grid.cpp

--- Minimum-Path-Sum-in-A-Triangle-Grid.cpp
+++ Minimum-Path-Sum-in-A-Triangle-Grid.cpp
@@ -1,4 +1,4 @@
-// Memoization TC - O(N*N) Sc - O(N*N)
+// Memoization TC - O(N*N) Sc - O(N*N), for recursive stack space , O(N*N) for dp array
 class Solution {
 public:
     int findPathSum(int i, int j, vector<vector<int>>&arr, vector<vector<int>>&dp,int N){
@@ -21,3 +21,22 @@ public:
 //.   3. 4
 //.   6. 5  7
 //.   4  1. 8. 3
+
+// Tabulation TC - O(N*N) Sc = O(N*N) 
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+     
+        for(int j = 0; j<n ; j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        for(int i = n-2 ;i>=0 ;i--){
+            for(int j = i ; j>=0; j--){
+                int down = INT_MAX,diagonal = INT_MAX;
+                down = triangle[i][j] + dp[i+1][j];
+                diagonal = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down,diagonal);
+            }
+        }
+        return dp[0][0];
+    }
